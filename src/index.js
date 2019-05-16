7ca4d1f5be240bd0eeaf181420335d3b809c6603
Jeremy Felt <jeremy.felt@gmail.com>
2019-05-15 19:26:29 -0700

    Remove double period from block description.

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ registerBlockType( 'happyprime/latest-custom-posts', {
 
 	title: __( 'Latest Custom Posts' ),
 
-	description: __( 'A list of posts for a custom post type and/or taxonomy..' ),
+	description: __( 'A list of posts for a custom post type and/or taxonomy.' ),
 
 	icon: <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false"><path d="M0,0h24v24H0V0z" fill="none"></path><rect x="11" y="7" width="6" height="2"></rect><rect x="11" y="11" width="6" height="2"></rect><rect x="11" y="15" width="6" height="2"></rect><rect x="7" y="7" width="2" height="2"></rect><rect x="7" y="11" width="2" height="2"></rect><rect x="7" y="15" width="2" height="2"></rect><path d="M20.1,3H3.9C3.4,3,3,3.4,3,3.9v16.2C3,20.5,3.4,21,3.9,21h16.2c0.4,0,0.9-0.5,0.9-0.9V3.9C21,3.4,20.5,3,20.1,3z M19,19H5V5h14V19z"></path></svg>,
 
