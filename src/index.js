68b944cf373afaaee110e8c95287a8406f13f6fd
philcable <phil@happyprime.co>
2019-11-13 10:05:32 -0800

    Update help text for selecting multiple terms

--- src/index.js
+++ src/index.js
@@ -447,7 +447,7 @@ registerBlockType( 'happyprime/latest-custom-posts', {
 							<SelectControl
 								multiple
 								label="Term(s)"
-								help="Cmd or Ctrl + click to select multiple terms"
+								help="Ctrl/Cmd + click to select multiple terms"
 								value={ taxonomy.terms }
 								options={ terms[ index ] }
 								onChange={ ( value ) => {
