661f706a231ab397bb8c448429e50020c41f0eed
philcable <phil@happyprime.co>
2021-07-06 14:02:30 -0700

    Add transforms for the Latest Post and Latest Custom Posts blocks

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { SVG, Path } from '@wordpress/primitives';
 // Internal dependencies
 import edit from './edit';
 
+import transforms from './transforms';
+
 // Block registration
 registerBlockType( 'happyprime/content-aggregator', {
 	icon: (
@@ -14,6 +16,8 @@ registerBlockType( 'happyprime/content-aggregator', {
 			<Path d="M20.1 3H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM19 19H5V5h14v14z" />
 		</SVG>
 	),
+
+	transforms,
 
 	edit,
 } );
---
+++ src/transforms.js
@@ -0,0 +1,66 @@
+// WordPress dependencies
+import { createBlock } from '@wordpress/blocks';
+
+const transforms = {
+	from: [
+		{
+			type: 'block',
+			blocks: [ 'core/latest-posts' ],
+			transform: ( attributes ) => {
+				return createBlock( 'happyprime/content-aggregator', {
+					itemCount: attributes.postsToShow,
+					order: attributes.order,
+					orderBy: attributes.orderBy,
+					displayPostDate: attributes.displayPostDate,
+					postLayout: attributes.postLayout,
+					columns: attributes.columns,
+					displayPostContent: attributes.displayPostContent,
+					postContent: attributes.displayPostContentRadio,
+					excerptLength: attributes.excerptLength,
+					displayImage: attributes.displayFeaturedImage,
+					imageSize: attributes.featuredImageSizeSlug,
+					stickyPosts: false,
+				} );
+			},
+		},
+		{
+			type: 'block',
+			blocks: [ 'happyprime/latest-custom-posts' ],
+			transform: ( attributes ) => {
+				const taxonomies =
+					attributes.taxonomies && 0 < attributes.taxonomies.length
+						? attributes.taxonomies
+						: ! attributes.customTaxonomy
+						? []
+						: 'string' !== typeof attributes.customTaxonomy
+						? attributes.customTaxonomy
+						: [
+								{
+									slug: attributes.customTaxonomy,
+									terms: [ `${ attributes.termID }` ],
+								},
+						  ];
+
+				return createBlock( 'happyprime/content-aggregator', {
+					customPostType: attributes.customPostType,
+					taxonomies,
+					taxRelation: attributes.taxRelation,
+					itemCount: attributes.itemCount,
+					order: attributes.order,
+					orderBy: attributes.orderBy,
+					displayPostDate: attributes.displayPostDate,
+					postLayout: attributes.postLayout,
+					columns: attributes.columns,
+					displayPostContent: attributes.displayPostContent,
+					postContent: attributes.postContent,
+					excerptLength: attributes.excerptLength,
+					displayImage: attributes.displayImage,
+					imageSize: attributes.imageSize,
+					stickyPosts: attributes.stickyPosts,
+				} );
+			},
+		},
+	],
+};
+
+export default transforms;
