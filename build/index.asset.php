efe283e62e6bb52c7afe5e2c13ba85c9f1cdea15
philcable <phil@happyprime.co>
2022-04-11 11:08:25 -0700

    Adjust plugin structure to improve build cleanliness

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '4f8232d96d2bdc98a770df1fcfb18414');
