baad405a1ee4e879374b196a7968fc241fa21052
Jeremy Felt <jeremy.felt@gmail.com>
2022-11-04 10:53:26 -0700

    Add `overflow: auto` to term selection interface

--- build/content-aggregator.asset.php
+++ build/content-aggregator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'c81e751155ba843544b4');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a3304adbc4216d9c45f5');
