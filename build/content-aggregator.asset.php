70be1f4f70d149779e03eff84c414690b9d0d5ea
Jeremy Felt <jeremy.felt@gmail.com>
2023-05-08 16:20:47 -0700

    Add option to sort results by menu_order

--- build/content-aggregator.asset.php
+++ build/content-aggregator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '5cb84ab013c0edd751e3');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '152fdb3ed1971ad4d3d4');
