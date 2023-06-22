eaed24fa4fd09f164478bc41ab48d0598022e25d
Jeremy Felt <jeremy.felt@gmail.com>
2023-06-22 09:06:07 -0700

    s/clasNames/className/ for author class attribute
    Fixes #109

--- build/content-aggregator.asset.php
+++ build/content-aggregator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a8966bc3ac0ef3683ac1');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'fc2288cc540505566e1f');
